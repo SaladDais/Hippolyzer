6dd0012b68593fe636dfd6f30838a1aeec906af5
locklainn.linden <devnull@localhost>
2008-08-06 20:41:20 +0000

    actual socket sending test, for teleport

--- pyogp/lib/base/network/net.py
+++ pyogp/lib/base/network/net.py
@@ -16,11 +16,12 @@ class NetUDPClient(object):
         return Host(self.sender.host, self.sender.port)
     
     def send_packet(self, sock, send_buffer, host):
-        sock.sendto(send_buffer, (host.ip, host.port))
+        print 'Sending to: ' + str(host.ip) + ":" + str(host.port)
+        bytes = sock.sendto(send_buffer, (host.ip, host.port))
     
-    def receive_packet(self, socket):
+    def receive_packet(self, sock):
         buf = 10000
-        data, addr = socket.recvfrom(buf)
+        data, addr = sock.recvfrom(buf)
         self.sender.ip_addr = addr
         self.sender.port()
         return data, len(data)
@@ -28,6 +29,7 @@ class NetUDPClient(object):
     def start_udp_connection(self, port):
         """ Starts a udp connection, returning socket and port. """
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.settimeout(10)
         #error check - make sure sock is good
 
         #will probably be other setup for this
