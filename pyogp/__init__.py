0c5e9b67ea73f03238d936de35bf966d7c868540
enus.linden <devnull@localhost>
2008-09-16 06:26:17 +0000

    oh me, adding a bunch of license info to a bunch of files. i hope i don't break anything

--- pyogp/__init__.py
+++ pyogp/__init__.py
@@ -1,3 +1,23 @@
+"""
+@file __init__.py
+@date 2008-09-16
+Contributors can be viewed at:
+http://svn.secondlife.com/svn/linden/projects/2008/pyogp/CONTRIBUTORS.txt 
+
+$LicenseInfo:firstyear=2008&license=apachev2$
+
+Copyright 2008, Linden Research, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License").
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+or in 
+http://svn.secondlife.com/svn/linden/projects/2008/pyogp/LICENSE.txt
+
+$/LicenseInfo$
+"""
+
 # See http://peak.telecommunity.com/DevCenter/setuptools#namespace-packages
 try:
     __import__('pkg_resources').declare_namespace(__name__)
