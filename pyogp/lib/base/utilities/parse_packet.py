e9cee56d611208999f52ba97c890623fb4f36e2f
enus.linden <devnull@localhost>
2009-02-09 23:24:48 +0000

    updated with wireshark pdml parsing, needs a refactor kinda bad, but works

--- pyogp/lib/base/utilities/parse_packet.py
+++ pyogp/lib/base/utilities/parse_packet.py
@@ -5,11 +5,52 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(sys.argv[0]), '..',
 
 from optparse import OptionParser
 import binascii
+import re
+import traceback
 from pyogp.lib.base.message.udpdeserializer import UDPPacketDeserializer
 from logging import getLogger, StreamHandler, Formatter, CRITICAL, ERROR, WARNING, INFO, DEBUG
 
 logger = getLogger('parse_packet')
 log = logger.log
+
+class parsingStats():
+
+#Todo: for this standalone script, move this class instance to global, move logging to the class itself
+
+    def __init__(self):
+
+        self.total = 0
+        self.success = 0
+        self.fail = 0
+        self.packets = {}
+        self.failed = []
+
+    def addSuccess(self, name):
+        self.success = self.success + 1
+        if self.packets.has_key(name):
+            self.packets[name] = self.packets[name] + 1
+        else:
+            self.packets[name] = 1
+
+    def addFail(self, data):
+        self.fail = self.fail + 1
+        self.failed.append(data)
+
+    def __repr__(self):
+        
+        string = "\n\n-----------------------------\n"
+        string = string + "          Parsing Summary\n"
+        string = string + "-----------------------------\n\n"
+        string = string + "Successes\n\n"
+        string = string + 'Parsed: %s\n\n' % (self.success)
+        for k in self.packets:
+            string = string + '%s: %s\n' %(k, self.packets[k])
+        string = string + "\n\nFailed to Parse\n\n"
+        string = string + 'Failed: %s\n\n' % (self.fail)       
+        for item in self.failed:
+            string = string + '%s\n' %(item)
+
+        return string
 
 
 def main():
@@ -19,31 +60,84 @@ def main():
     if options.verbose: enable_logging()
 
     if options.file:
-        process_file(options.file)
+        stats = parsingStats()
+        process_file(options, stats)
+        print stats
     else:
         process_stream(options.data)
 
-def process_file(file):
+def process_file(options, stats):
 
-    handle = open(file,"r")
+    if options.wireshark:
+        handle = open(options.file,"r")
+        # normal hex file, 1 packet of hex per line    
+        lines = handle.readlines
+        handle.close()
+        isSLclientpacket = False
+        isSLsimpacket = False
+        packetSource = ''
+
+        if options.count > 0:
+            counter = 0.0
+            linecount = len(lines)
+            markers = [10,20,30,40,50,60,70,80,90,100]
+
+            log(INFO, 'Processing %s of up to %s packets' % (options.count, linecount))
+
+        percent = 0.0
+        for line in lines:
+            if percent == 100: break
+            if re.search('udp.srcport', line) and re.search('1300[0-9]', line):
+                isSLsimpacket = True
+                packetSource = 'Simulator'
+                continue
+            if re.search('udp.dstport', line) and re.search('1300[0-9]', line):
+                isSLclientpacket = True
+                packetSource = 'Client'
+                continue 
+            if (isSLclientpacket == True or isSLsimpacket == True):
+                if re.search('field name="data"', line):
+                    if options.count > 0:
+                        percent = (counter/float(options.count)) * 100
+                        #log(INFO, "%s percent complete... (%s of %s)" % (percent, counter, options.count))
+                        if (percent) in markers:
+                            log(INFO, "%s percent complete... (%s of %s)" % (int(percent), counter, options.count))
+                    counter += 1
+                    items = line.split()
+                    process_stream(items[2][7:len(items[2])-3], packetSource, stats)
+                    isSLclientpacket = False
+                    isSLsimpacket = False
+                       
+    else:
+        handle = open(options.file,"r")
+        # normal hex file, 1 packet of hex per line    
+        lines = handle.readlines() 
+        for line in lines:
+            process_stream(line.strip())
+
+def process_wiresharkpacket(packet):
+
+    print packet.__dict__
     
-    lines = handle.readlines()
-    for line in lines:
-        process_stream(line.strip())
 
-
-def process_stream(data):
+def process_stream(data, source=None, stats = None):
 
     msg_buff = gen_message_buffer(data)
-    
-    deserializer = UDPPacketDeserializer(msg_buff)
-    packet = deserializer.deserialize()
 
-    #print packet.__dict__
-    try:
-        display_packet(packet, data)
-    except AttributeError:
-        print 'Unable to parse hex stream'
+    if msg_buff != None:
+        try:
+            deserializer = UDPPacketDeserializer(msg_buff)
+            packet = deserializer.deserialize()
+            display_packet(packet, data, source)
+            if stats != None:
+                stats.addSuccess(packet.name)
+        except Exception, e:
+            print 'Unable to parse data "%s" due to: %s' % (data, e)
+            traceback.print_exc()
+            if stats != None:
+                stats.addFail(data)
+    else:
+        print 'Skipping empty message buffer'
 
 def parse_options():
 
@@ -53,8 +147,14 @@ def parse_options():
     parser.add_option("-d", "--data", dest="data", help="data to parse")
     parser.add_option("-v", "--verbose", dest="verbose", default=True, action="store_false")
     parser.add_option("-f", "--file", dest="file", help="parse data located in file")
+    parser.add_option("-w", "--wireshark", dest="wireshark", default=False, action="store_true", help="specifies file to parse is a wireshark pdml dump")
+    parser.add_option("-c", "--count", dest="count", default=0, help="how many packets to process from a file")
 
     (options, args) = parser.parse_args()
+
+    if options.wireshark and not options.file:
+        print "Cannot parse as wireshark and not pass a file in..."
+        sys.exit(-1)
 
     return options
 
@@ -67,28 +167,41 @@ def gen_message_buffer(data, datatype='hex'):
 
 def message_buff_from_hex(data):
 
-    return binascii.unhexlify(''.join(data.split()))
+    try:
+        parsed = binascii.unhexlify(''.join(data.split()))
+        return parsed
+    except:
+        print 'Error parsing data: %s' % (data)
+        return None
 
-def display_packet(packet, data):
+def display_packet(packet, data, source=None):
+
+    sourcestring = ''
+    if source != None:
+        sourcestring = ' (from %s)' % (source)
 
     print "~~~~~~~~~~~~~~~~~~~"
-    print "Source data:"
+    print "Source data%s:" % (sourcestring)
     print data
     print "~~~~~~~~~~~~~~~~~~~"
     delim = "    "
     print 'Packet Name:%s%s' % (delim, packet.name)
 
     for k in packet.__dict__:
+        
         if k == 'name': pass
         if k == 'message_data':
             print k
             for ablock in packet.message_data.blocks:
                 print "%sBlock Name:%s%s" % (delim, delim, ablock)
-       
                 for somevars in packet.message_data.blocks[ablock]:
+                    #print somevars.var_list
                     for avar in somevars.var_list:
+                        #print somevars.var_list
+                        #print somevars.vars
                         zvar = somevars.get_variable(avar)
-                        print "%s%s%s:%s%s" % (delim, delim, zvar.name, delim, zvar.get_data_as_string())
+                        print "%s%s%s:%s%s" % (delim, delim, zvar.name, delim, zvar)
+                        #print zvar.data
     print "~~~~~~~~~~~~~~~~~~~"
 
         #print '%s:\t%s' % (k, packet.__dict__[k])
@@ -100,6 +213,8 @@ def display_packet(packet, data):
             print 'Block Name:\t%s' % (v.name)
             for k, v in getattr(packet, k):
     '''               
+
+    
 
 def enable_logging():
 
