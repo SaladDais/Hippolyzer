16beb81c96339007de35d147899b7f97b7875e9b
enus.linden <devnull@localhost>
2009-03-11 03:57:42 +0000

    the simple object creation entry point

---
+++ pyogp/lib/base/examples/sample_object_creation.py
@@ -0,0 +1,124 @@
+#!/usr/bin/python
+"""
+@file sample_object_creation.py
+@date 2009-03-09
+Contributors can be viewed at:
+http://svn.secondlife.com/svn/linden/projects/2008/pyogp/CONTRIBUTORS.txt
+
+$LicenseInfo:firstyear=2008&license=apachev2$
+
+Copyright 2008, Linden Research, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License").
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+or in
+http://svn.secondlife.com/svn/linden/projects/2008/pyogp/LICENSE.txt
+
+$/LicenseInfo$
+"""
+
+# standard
+import re
+import getpass, sys, logging
+from optparse import OptionParser
+
+# related
+from eventlet import api
+
+# pyogp
+from pyogp.lib.base.agent import Agent
+from pyogp.lib.base.settings import Settings
+
+
+def login():
+   """ login an to a login endpoint """
+
+   parser = OptionParser()
+
+   logger = logging.getLogger("pyogp.lib.base.example")
+
+   parser.add_option("-l", "--loginuri", dest="loginuri", default="https://login.aditi.lindenlab.com/cgi-bin/login.cgi",
+                     help="specified the target loginuri")
+   parser.add_option("-r", "--region", dest="region", default=None,
+                     help="specifies the region to connect to")
+#http://ec2-75-101-203-98.compute-1.amazonaws.com:9000
+   parser.add_option("-q", "--quiet", dest="verbose", default=True, action="store_false",
+                   help="enable verbose mode")
+
+
+   (options, args) = parser.parse_args()
+
+   if options.verbose:
+       console = logging.StreamHandler()
+       console.setLevel(logging.DEBUG) # seems to be a no op, set it for the logger
+       formatter = logging.Formatter('%(asctime)-30s%(name)-30s: %(levelname)-8s %(message)s')
+       console.setFormatter(formatter)
+       logging.getLogger('').addHandler(console)
+
+       # setting the level for the handler above seems to be a no-op
+       # it needs to be set for the logger, here the root logger
+       # otherwise it is NOTSET(=0) which means to log nothing.
+       logging.getLogger('').setLevel(logging.DEBUG)
+   else:
+       print "Attention: This script will print nothing if you use -q. So it might be boring to use it like that ;-)"
+
+   # example from a pure agent perspective
+
+   #grab a password!
+   password = getpass.getpass()
+
+   # let's disable inventory handling for this example
+   settings = Settings()
+   settings.ENABLE_INVENTORY_MANAGEMENT = False
+
+   #First, initialize the agent
+   client = Agent(settings = settings)
+
+   # Now let's log it in
+   api.spawn(client.login, options.loginuri, args[0], args[1], password, start_location = options.region, connect_region = True)
+
+   # wait for the agent to connect to it's region
+   while client.connected == False:
+       api.sleep(0)
+
+   while client.region.connected == False:
+       api.sleep(0)
+
+   # in this case, wait for the client.Position to become populated, as we need to rez a box
+   # relative to our current position
+   while client.Position == None:
+       api.sleep(0)
+
+   # do sample script specific stuff here
+
+   client.region.objects.create_default_box()
+
+   while client.running:
+       api.sleep(0)
+
+   print ''
+   print ''
+   print 'At this point, we have an Agent object, Inventory dirs, and with a Region attribute'
+   print 'Agent attributes:'
+   for attr in client.__dict__:
+       print attr, ':\t\t\t',  client.__dict__[attr]
+   print ''
+   print ''
+   print 'Objects being tracked: %s' % len(client.region.objects.object_store)
+   print ''
+   print ''
+   for _object in client.region.objects.object_store:
+       print 'ID:', _object.ID, '\tUUID: ', _object.FullID
+   print ''
+   print ''
+   print 'Region attributes:'
+   for attr in client.region.__dict__:
+       print attr, ':\t\t\t',  client.region.__dict__[attr]
+
+def main():
+   return login()
+
+if __name__=="__main__":
+   main()
