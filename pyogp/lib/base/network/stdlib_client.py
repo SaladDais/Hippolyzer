b1085a74a4ed912d1efcf4ef62b797887f3b94c6
joshua.linden <devnull@localhost>
2009-09-30 23:31:43 +0000

    Remove unused EventletClient which requires eventlet.httpc, which is removed in newer versions of eventlet

--- pyogp/lib/base/network/stdlib_client.py
+++ pyogp/lib/base/network/stdlib_client.py
@@ -19,18 +19,6 @@ $/LicenseInfo$
 import urllib2
 import os
 
-# eventlet
-import sys
-lib_dir = os.path.abspath(os.path.join(os.path.dirname(sys.argv[0]), '..', '..', 'src/lib'))
-if lib_dir not in sys.path:
-    sys.path.insert(0, lib_dir)
-
-try:
-    from eventlet import api, httpc
-except ImportError:
-    print "Error importing eventlet"
-    sys.exit()
-
 from pyogp.lib.base.exc import HTTPError
 
 from webob import Request, Response
@@ -67,41 +55,4 @@ class StdLibClient(object):
         status = "%s %s" %(result.code, result.msg)
         response = Response(body = result.read(), status = status, headerlist = headerlist)
         return response
-
-# really raw, got a lot to do
-class EventletClient(object):
-    """ implement a REST client on top of eventlet.httpc """
-
-    def GET(self, url, headers={}):
-        """ GET a resource """ 
-
-        #request = urllib2.Request(url, headers=headers)
-        try:
-            result = httpc.get(url, headers=headers)
-        except Exception, e:
-            raise e
-
-        # convert back to webob
-        headerlist = result.headers.items()
-        status = "%s %s" %(result.code, result.msg)
-        response = Response(body = result.read(), status = status, headerlist = headerlist)
-        return response
-
-    def POST(self, url, data, headers={}):
-        """ POST data to a resource """
-
-        #request = urllib2.Request(url, data, headers=headers)
-        try:
-            (status, msg, body) = httpc.post_(url, data=data, headers=headers)
-            print msg.__dict__
-        except Exception, e:
-            raise e
-
-        # convert back to webob
-        headerlist = msg.dict
-        status = "%s %s" %(status, msg.status)
-        response = Response(body = body, status = status, headerlist = headerlist)
-        return response
-
-
 
