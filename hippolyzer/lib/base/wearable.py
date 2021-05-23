c9138b46497e69a515334f200c2b23d9ab69bbe0
Salad Dais <SaladDais@users.noreply.github.com>
2021-05-23 04:40:48 +0000

    Add wearable asset serialization support

---
+++ hippolyzer/lib/base/wearable.py
@@ -0,0 +1,104 @@
+"""
+Common format used by all bodyparts and linden clothing layers
+"""
+
+from __future__ import annotations
+
+import dataclasses
+import logging
+from io import StringIO
+from typing import *
+
+from hippolyzer.lib.base.datatypes import UUID
+from hippolyzer.lib.base.legacy_inv import InventorySaleInfo, InventoryPermissions
+from hippolyzer.lib.base.legacy_schema import SchemaBase, parse_schema_line, SchemaParsingError
+
+LOG = logging.getLogger(__name__)
+_T = TypeVar("_T")
+
+WEARABLE_VERSION = "LLWearable version 22"
+
+
+@dataclasses.dataclass
+class Wearable(SchemaBase):
+    name: str
+    wearable_type: int
+    permissions: InventoryPermissions
+    sale_info: InventorySaleInfo
+    # VisualParam ID -> val
+    parameters: Dict[int, float]
+    # TextureEntry ID -> texture ID
+    textures: Dict[int, UUID]
+
+    @classmethod
+    def _skip_to_next_populated_line(cls, reader: StringIO):
+        old_pos = reader.tell()
+        while peeked_data := reader.readline():
+            # Read until we find a non-blank line
+            if peeked_data.lstrip("\n"):
+                break
+            old_pos = reader.tell()
+        # Reading an empty string means EOF
+        if not peeked_data:
+            raise SchemaParsingError("Premature EOF")
+        reader.seek(old_pos)
+
+    @classmethod
+    def _read_and_parse_line(cls, reader: StringIO):
+        cls._skip_to_next_populated_line(reader)
+        return parse_schema_line(reader.readline())
+
+    @classmethod
+    def _read_expected_key(cls, reader: StringIO, expected_key: str) -> str:
+        key, val = cls._read_and_parse_line(reader)
+        if key != expected_key:
+            raise ValueError(f"Expected {expected_key} not found, {(key, val)!r}")
+        return val
+
+    @classmethod
+    def from_reader(cls, reader: StringIO) -> Wearable:
+        cls._skip_to_next_populated_line(reader)
+        version_str = reader.readline().rstrip()
+        if version_str != WEARABLE_VERSION:
+            raise ValueError(f"Bad wearable version {version_str!r}")
+        cls._skip_to_next_populated_line(reader)
+        name = reader.readline().rstrip()
+
+        permissions = InventoryPermissions.from_reader(reader, read_header=True)
+        sale_info = InventorySaleInfo.from_reader(reader, read_header=True)
+
+        wearable_type = int(cls._read_expected_key(reader, "type"))
+        num_params = int(cls._read_expected_key(reader, "parameters"))
+        params = {}
+        for _ in range(num_params):
+            param_id, param_val = cls._read_and_parse_line(reader)
+            if param_val == ".":
+                param_val = "0.0"
+            params[int(param_id)] = float(param_val)
+
+        num_textures = int(cls._read_expected_key(reader, "textures"))
+        textures = {}
+        for _ in range(num_textures):
+            te_id, texture_id = cls._read_and_parse_line(reader)
+            textures[int(te_id)] = UUID(texture_id)
+        return Wearable(
+            name=name,
+            wearable_type=wearable_type,
+            permissions=permissions,
+            sale_info=sale_info,
+            parameters=params,
+            textures=textures
+        )
+
+    def to_writer(self, writer: StringIO):
+        writer.write(f"{WEARABLE_VERSION}\n")
+        writer.write(f"{self.name}\n\n")
+        self.permissions.to_writer(writer)
+        self.sale_info.to_writer(writer)
+        writer.write(f"type {self.wearable_type}\n")
+        writer.write(f"parameters {len(self.parameters)}\n")
+        for param_id, param_val in self.parameters.items():
+            writer.write(f"{param_id} {param_val}\n")
+        writer.write(f"textures {len(self.textures)}\n")
+        for te_id, texture_id in self.textures.items():
+            writer.write(f"{te_id} {texture_id}\n")
--- tests/base/test_legacy_schema.py
+++ tests/base/test_legacy_schema.py
@@ -2,6 +2,7 @@ import unittest
 
 from hippolyzer.lib.base.datatypes import *
 from hippolyzer.lib.base.legacy_inv import InventoryModel
+from hippolyzer.lib.base.wearable import Wearable
 
 SIMPLE_INV = """\tinv_object\t0
 \t{
@@ -59,3 +60,123 @@ class TestLegacyInv(unittest.TestCase):
         self.assertEqual(item.name, "New Script")
         self.assertEqual(item.sale_info.sale_type, "not")
         self.assertEqual(item.model, model)
+
+
+GIRL_NEXT_DOOR_SHAPE = """LLWearable version 22
+Girl Next Door - C2 - med - Adam n Eve
+
+\tpermissions 0
+\t{
+\t\tbase_mask\t0008e000
+\t\towner_mask\t0008e000
+\t\tgroup_mask\t00000000
+\t\teveryone_mask\t00000000
+\t\tnext_owner_mask\t0008e000
+\t\tcreator_id\tdf110c10-72e6-40d6-8916-14b4b0366b18
+\t\towner_id\t285a8af7-7a1d-4608-8fb5-200b816e7ee1
+\t\tlast_owner_id\t285a8af7-7a1d-4608-8fb5-200b816e7ee1
+\t\tgroup_id\t00000000-0000-0000-0000-000000000000
+\t}
+\tsale_info\t0
+\t{
+\t\tsale_type\tnot
+\t\tsale_price\t10
+\t}
+type 0
+parameters 82
+1 -.21
+2 -.4
+4 -.08
+5 .04
+6 -.04
+7 .02
+8 -.42
+10 -.15
+11 .16
+12 -.5
+13 0
+14 1
+15 -.16
+17 .17
+18 -.42
+19 -.78
+20 -.16
+21 .01
+22 0
+23 -.44
+24 .32
+25 .35
+27 -.55
+33 -.11
+34 .22
+35 -.1
+36 -1.06
+37 -.86
+38 -.42
+80 0
+105 .59
+155 -.24
+157 0
+185 -.68
+193 .55
+196 -.98
+505 .39
+506 -.68
+507 -.77
+515 -1
+517 -.11
+518 .6
+629 .5
+637 .24
+646 -.26
+647 -.05
+649 .36
+650 .25
+652 .4
+653 .29
+656 0
+659 .76
+662 .5
+663 0
+664 0
+665 .08
+675 -.14
+676 -.58
+678 .5
+682 .5
+683 -.17
+684 .28
+685 0
+690 .42
+692 .26
+693 -.12
+753 .51
+756 -.2
+758 .21
+759 .5
+760 .85
+764 .32
+765 .18
+769 .37
+773 .53
+795 .22
+796 0
+799 .5
+841 -.12
+842 0
+879 0
+880 -.37
+textures 0
+"""
+
+
+class TestWearable(unittest.TestCase):
+    def test_parse(self):
+        wearable = Wearable.from_str(GIRL_NEXT_DOOR_SHAPE)
+        self.assertEqual(wearable.name, "Girl Next Door - C2 - med - Adam n Eve")
+        self.assertEqual(wearable.parameters[841], -.12)
+
+    def test_serialize(self):
+        wearable = Wearable.from_str(GIRL_NEXT_DOOR_SHAPE)
+        new_wearable = Wearable.from_str(wearable.to_str())
+        self.assertEqual(wearable, new_wearable)
