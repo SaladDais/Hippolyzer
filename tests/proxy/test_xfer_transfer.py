960c8aa90559ee7815145a786bdfb55bf62b7714
Salad Dais <SaladDais@users.noreply.github.com>
2021-06-02 05:38:55 +0000

    Add test for large xfer payload case

--- tests/proxy/test_xfer_transfer.py
+++ tests/proxy/test_xfer_transfer.py
@@ -3,7 +3,7 @@ from typing import *
 
 from hippolyzer.lib.base.message.message import Block
 from hippolyzer.lib.base.message.message_handler import MessageHandler
-from hippolyzer.lib.base.templates import AssetType
+from hippolyzer.lib.base.templates import AssetType, XferFilePath, XferPacket
 from hippolyzer.lib.proxy.circuit import ProxiedCircuit
 from hippolyzer.lib.proxy.message import ProxiedMessage
 from hippolyzer.lib.proxy.packets import Direction
@@ -23,6 +23,9 @@ class MockHandlingCircuit(ProxiedCircuit):
 
 
 class XferManagerTests(BaseProxyTest):
+    SMALL_PAYLOAD = b"foobar"
+    LARGE_PAYLOAD = b"foobar" * 500
+
     def setUp(self) -> None:
         super().setUp()
         self._setup_default_circuit()
@@ -37,20 +40,61 @@ class XferManagerTests(BaseProxyTest):
         asset_block = msg["AssetBlock"]
         transaction_id = asset_block["TransactionID"]
         asset_id = self.session.tid_to_assetid(transaction_id)
+        client_handler = self.session.main_region.message_handler
         if asset_block["AssetData"]:
             self.received_bytes = asset_block["AssetData"]
-            self.session.main_region.message_handler.handle(ProxiedMessage(
-                "AssetUploadComplete",
-                Block("AssetBlock", UUID=asset_id, Type=asset_block["Type"], Success=True),
-                direction=Direction.IN,
-            ))
         else:
-            pass
+            with self.message_handler.subscribe_async(("SendXferPacket",)) as get_msgs:
+                client_handler.handle(ProxiedMessage(
+                    'RequestXfer',
+                    Block(
+                        'XferID',
+                        ID=2,
+                        Filename=b'',
+                        FilePath=XferFilePath.NONE,
+                        DeleteOnCompletion=False,
+                        UseBigPackets=False,
+                        VFileID=asset_id,
+                        VFileType=AssetType.BODYPART,
+                    ),
+                    direction=Direction.IN,
+                ))
+                self.received_bytes = b""
+                while True:
+                    msg = await asyncio.wait_for(get_msgs(), 0.005)
+                    packet_id: XferPacket = msg["XferID"][0].deserialize_var("Packet")
+                    data = msg["DataPacket"]["Data"]
+                    # First 4 bytes are expected len
+                    if packet_id.PacketID == 0:
+                        data = data[4:]
+                    self.received_bytes += data
+                    client_handler.handle(ProxiedMessage(
+                        "ConfirmXferPacket",
+                        Block("XferID", ID=2, Packet=packet_id.PacketID),
+                        direction=Direction.IN,
+                    ))
+                    if packet_id.IsEOF:
+                        break
+        await asyncio.sleep(0.005)
+        client_handler.handle(ProxiedMessage(
+            "AssetUploadComplete",
+            Block("AssetBlock", UUID=asset_id, Type=asset_block["Type"], Success=True),
+            direction=Direction.IN,
+        ))
 
-    async def test_short_xfer_upload(self):
+    async def test_small_xfer_upload(self):
         asyncio.create_task(self._handle_upload())
         xfer = asyncio.wait_for(self.xfer_manager.upload_asset(
-            AssetType.BODYPART, b"foobar"
+            AssetType.BODYPART, self.SMALL_PAYLOAD
         ), timeout=0.05)
         await xfer
-        self.assertEqual(self.received_bytes, b"foobar")
+        self.assertEqual(self.received_bytes, self.SMALL_PAYLOAD)
+
+    async def test_large_xfer_upload(self):
+        # Larger payloads take a different path
+        asyncio.create_task(self._handle_upload())
+        xfer = asyncio.wait_for(self.xfer_manager.upload_asset(
+            AssetType.BODYPART, self.LARGE_PAYLOAD
+        ), timeout=0.05)
+        await xfer
+        self.assertEqual(self.received_bytes, self.LARGE_PAYLOAD)
