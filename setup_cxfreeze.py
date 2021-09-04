0c0de2bcbcde24d34012151f14a076c671cb9e27
Salad Dais <SaladDais@users.noreply.github.com>
2021-09-04 07:27:20 +0000

    v0.7.1

--- setup_cxfreeze.py
+++ setup_cxfreeze.py
@@ -112,7 +112,7 @@ executables = [
 
 setup(
     name="hippolyzer_gui",
-    version="0.7.0",
+    version="0.7.1",
     description="Hippolyzer GUI",
     options=options,
     executables=executables,
