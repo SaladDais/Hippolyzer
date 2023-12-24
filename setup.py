ca5c71402b045a6f06fe9e77507cff21f747f7f7
Salad Dais <SaladDais@users.noreply.github.com>
2023-12-24 05:57:14 +0000

    Bump Python requirement to 3.9

--- setup.py
+++ setup.py
@@ -42,7 +42,6 @@ setup(
         "Operating System :: POSIX",
         "Operating System :: Microsoft :: Windows",
         "Programming Language :: Python :: 3 :: Only",
-        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
@@ -80,7 +79,7 @@ setup(
         }
     },
     zip_safe=False,
-    python_requires='>=3.8',
+    python_requires='>=3.9',
     install_requires=[
         'llsd<1.1.0',
         'defusedxml',
