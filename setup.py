6dafe32f6ac060aab549915a7ae393bee628f087
Salad Dais <SaladDais@users.noreply.github.com>
2025-04-18 04:33:00 +0000

    Update version to v0.15.6
    I forgot I have to manually do it in this repo.

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ from setuptools import setup, find_packages
 
 here = path.abspath(path.dirname(__file__))
 
-version = '0.15.2'
+version = '0.15.6'
 
 with open(path.join(here, 'README.md')) as readme_fh:
     readme = readme_fh.read()
